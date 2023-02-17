5b6ef3744a7eb7cbc153555aa02814927ce6c33e
hasan <khasan_sidikov@epam.com>
2023-02-18 00:28:16 +0500

    MODELINKS-34: Test fix

--- src/test/java/org/folio/entlinks/service/messaging/authority/model/AuthorityChangeHolderTest.java
+++ src/test/java/org/folio/entlinks/service/messaging/authority/model/AuthorityChangeHolderTest.java
@@ -272,7 +272,8 @@ class AuthorityChangeHolderTest {
     String updatedByUserId = startedByUserId.toString();
     var holder = new AuthorityChangeHolder(
       new InventoryEvent().type(InventoryEventType.UPDATE.toString())
-        ._new(new AuthorityInventoryRecord().naturalId("n").metaData(new org.folio.entlinks.domain.dto.MetaData().updatedByUserId(updatedByUserId)))
+        ._new(new AuthorityInventoryRecord().naturalId("n")
+          .metaData(new org.folio.entlinks.domain.dto.MetaData().updatedByUserId(updatedByUserId)))
         .old(new AuthorityInventoryRecord().naturalId("o")),
       Map.of(PERSONAL_NAME, new AuthorityChange(PERSONAL_NAME, "n", "o"),
         NATURAL_ID, new AuthorityChange(NATURAL_ID, "n", "o")),
