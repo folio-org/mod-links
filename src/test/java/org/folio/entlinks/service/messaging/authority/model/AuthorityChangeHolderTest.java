367628374e61b1a659eff8f5e69b0dc86a4b7ee2
hasan <khasan_sidikov@epam.com>
2023-02-20 14:37:21 +0500

    MODELINKS-34: Tests fixed

--- src/test/java/org/folio/entlinks/service/messaging/authority/model/AuthorityChangeHolderTest.java
+++ src/test/java/org/folio/entlinks/service/messaging/authority/model/AuthorityChangeHolderTest.java
@@ -6,9 +6,7 @@ import static org.folio.entlinks.service.messaging.authority.model.AuthorityChan
 import static org.folio.entlinks.service.messaging.authority.model.AuthorityChangeField.PERSONAL_NAME;
 import static org.folio.entlinks.service.messaging.authority.model.AuthorityChangeField.PERSONAL_NAME_TITLE;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Map;
 import java.util.UUID;
@@ -16,118 +14,16 @@ import org.folio.entlinks.domain.dto.AuthorityInventoryRecord;
 import org.folio.entlinks.domain.dto.InventoryEvent;
 import org.folio.entlinks.domain.dto.InventoryEventType;
 import org.folio.entlinks.domain.entity.AuthorityDataStatAction;
+import org.folio.spring.test.type.UnitTest;
 import org.junit.jupiter.api.Test;
 
+@UnitTest
 class AuthorityChangeHolderTest {
 
   public static final String[] STAT_OBJ_PROPERTIES =
     {"action", "headingOld", "headingNew", "headingTypeOld", "headingTypeNew",
      "authorityNaturalIdOld", "authorityNaturalIdNew", "authoritySourceFileOld", "authoritySourceFileNew",
      "lbTotal", "startedByUserId"};
-
-  @Test
-  void getNewNaturalId_positive() {
-    var holder = new AuthorityChangeHolder(
-      new InventoryEvent()
-        ._new(new AuthorityInventoryRecord().naturalId("n"))
-        .old(new AuthorityInventoryRecord().naturalId("o")),
-      Map.of(NATURAL_ID, new AuthorityChange(NATURAL_ID, "n", "o")),
-      Map.of(), 1);
-
-    var actual = holder.getNewNaturalId();
-
-    assertEquals("n", actual);
-  }
-
-  @Test
-  void getNewSourceFileId_positive() {
-    var sourceFileIdNew = UUID.randomUUID();
-    var sourceFileIdOld = UUID.randomUUID();
-    var holder = new AuthorityChangeHolder(
-      new InventoryEvent()
-        ._new(new AuthorityInventoryRecord().sourceFileId(sourceFileIdNew))
-        .old(new AuthorityInventoryRecord().sourceFileId(sourceFileIdOld)),
-      Map.of(NATURAL_ID, new AuthorityChange(NATURAL_ID, "n", "o")),
-      Map.of(), 1);
-
-    var actual = holder.getNewSourceFileId();
-
-    assertEquals(sourceFileIdNew, actual);
-  }
-
-  @Test
-  void isNaturalIdChanged_positive_naturalIdIsInChanges() {
-    var holder = new AuthorityChangeHolder(
-      new InventoryEvent()
-        ._new(new AuthorityInventoryRecord())
-        .old(new AuthorityInventoryRecord()),
-      Map.of(NATURAL_ID, new AuthorityChange(NATURAL_ID, "n", "o"),
-        PERSONAL_NAME, new AuthorityChange(PERSONAL_NAME, "n", "o")),
-      Map.of(), 1);
-
-    var actual = holder.isNaturalIdChanged();
-
-    assertTrue(actual);
-  }
-
-  @Test
-  void isNaturalIdChanged_positive_naturalIdIsNotInChanges() {
-    var holder = new AuthorityChangeHolder(
-      new InventoryEvent()
-        ._new(new AuthorityInventoryRecord())
-        .old(new AuthorityInventoryRecord()),
-      Map.of(PERSONAL_NAME_TITLE, new AuthorityChange(PERSONAL_NAME_TITLE, "n", "o"),
-        PERSONAL_NAME, new AuthorityChange(PERSONAL_NAME, "n", "o")),
-      Map.of(), 1);
-
-    var actual = holder.isNaturalIdChanged();
-
-    assertFalse(actual);
-  }
-
-  @Test
-  void isOnlyNaturalIdChanged_positive_onlyNaturalIdIsInChanges() {
-    var holder = new AuthorityChangeHolder(
-      new InventoryEvent()
-        ._new(new AuthorityInventoryRecord())
-        .old(new AuthorityInventoryRecord()),
-      Map.of(NATURAL_ID, new AuthorityChange(NATURAL_ID, "n", "o")),
-      Map.of(), 1);
-
-    var actual = holder.isOnlyNaturalIdChanged();
-
-    assertTrue(actual);
-  }
-
-  @Test
-  void isOnlyNaturalIdChanged_positive_notOnlyNaturalIdIsInChanges() {
-    var holder = new AuthorityChangeHolder(
-      new InventoryEvent()
-        ._new(new AuthorityInventoryRecord())
-        .old(new AuthorityInventoryRecord()),
-      Map.of(NATURAL_ID, new AuthorityChange(NATURAL_ID, "n", "o"),
-        PERSONAL_NAME, new AuthorityChange(PERSONAL_NAME, "n", "o")),
-      Map.of(), 1);
-
-    var actual = holder.isOnlyNaturalIdChanged();
-
-    assertFalse(actual);
-  }
-
-  @Test
-  void isOnlyNaturalIdChanged_positive_naturalIdIsNotInChanges() {
-    var holder = new AuthorityChangeHolder(
-      new InventoryEvent()
-        ._new(new AuthorityInventoryRecord())
-        .old(new AuthorityInventoryRecord()),
-      Map.of(PERSONAL_NAME_TITLE, new AuthorityChange(PERSONAL_NAME_TITLE, "n", "o"),
-        PERSONAL_NAME, new AuthorityChange(PERSONAL_NAME, "n", "o")),
-      Map.of(), 1);
-
-    var actual = holder.isOnlyNaturalIdChanged();
-
-    assertFalse(actual);
-  }
 
   @Test
   void getFieldChange_positive_onlyFieldChange() {
