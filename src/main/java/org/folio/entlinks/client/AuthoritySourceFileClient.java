ad92946da8b6e3a636eae487b85f44ec78feeeda
Shans Kaluhin <shans_kaluhin@epam.com>
2023-03-10 10:56:38 +0200

    Fix mappings

--- src/main/java/org/folio/entlinks/client/AuthoritySourceFileClient.java
+++ src/main/java/org/folio/entlinks/client/AuthoritySourceFileClient.java
@@ -6,13 +6,14 @@ import java.util.List;
 import java.util.UUID;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient("authority-source-files")
 public interface AuthoritySourceFileClient {
 
 
   @GetMapping(produces = APPLICATION_JSON_VALUE)
-  AuthoritySourceFiles fetchAuthoritySourceFiles(int limit);
+  AuthoritySourceFiles fetchAuthoritySourceFiles(@RequestParam int limit);
 
   record AuthoritySourceFile(UUID id, String baseUrl, String name) { }
 
