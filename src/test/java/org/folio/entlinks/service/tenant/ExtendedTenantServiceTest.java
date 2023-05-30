961e58d6b942cf34273ecf162d4585108db28614
hasan <khasan_sidikov@epam.com>
2023-05-30 15:12:13 +0500

    MODELINKS-87: Added test case

--- src/test/java/org/folio/entlinks/service/tenant/ExtendedTenantServiceTest.java
+++ src/test/java/org/folio/entlinks/service/tenant/ExtendedTenantServiceTest.java
@@ -1,10 +1,23 @@
 package org.folio.entlinks.service.tenant;
 
+import static io.vertx.core.Future.succeededFuture;
 import static org.folio.support.base.TestConstants.TENANT_ID;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyCollection;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+import org.apache.kafka.clients.admin.KafkaAdminClient;
+import org.apache.kafka.clients.admin.ListTopicsResult;
+import org.apache.kafka.clients.admin.TopicListing;
+import org.apache.kafka.common.KafkaFuture;
+import org.apache.kafka.common.Uuid;
+import org.apache.kafka.common.internals.KafkaFutureImpl;
 import org.folio.spring.FolioExecutionContext;
 import org.folio.spring.test.type.UnitTest;
 import org.folio.spring.tools.kafka.KafkaAdminService;
@@ -27,6 +40,8 @@ class ExtendedTenantServiceTest {
   @Mock
   private KafkaAdminService kafkaAdminService;
   @Mock
+  private KafkaAdminClient kafkaAdminClient;
+  @Mock
   private PrepareSystemUserService prepareSystemUserService;
 
   @Test
@@ -41,6 +56,16 @@ class ExtendedTenantServiceTest {
     verify(prepareSystemUserService).setupSystemUser();
     verify(kafkaAdminService).createTopics(TENANT_ID);
     verify(kafkaAdminService).restartEventListeners();
+  }
+
+  @Test
+  void deleteTopicAfterTenantDeletion() {
+    ListTopicsResult topicsResult = new ListTopicsResult(new KafkaFutureImpl<>());
+    when(kafkaAdminClient.listTopics()).thenReturn(topicsResult);
+
+    tenantService.afterTenantDeletion(tenantAttributes());
+
+    verify(kafkaAdminService).deleteTopics(anyCollection());
   }
 
   private TenantAttributes tenantAttributes() {
