56f38556bee759278e142bbc080b8c3aac9f35be
Shans Kaluhin <shans_kaluhin@epam.com>
2023-06-13 16:22:12 +0300

    Add documentation for FieldUtils

--- src/main/java/org/folio/entlinks/utils/FieldUtils.java
+++ src/main/java/org/folio/entlinks/utils/FieldUtils.java
@@ -11,7 +11,13 @@ import org.folio.entlinks.client.AuthoritySourceFileClient.AuthoritySourceFile;
 @UtilityClass
 public class FieldUtils {
 
-  public static String trimZeroValue(String zeroValue) {
+  /**
+   * Extract naturalId from subfield $0.
+   *
+   * @param zeroValue subfield $0 value as {@link String}
+   * @return trimmed naturalId as {@link String}
+   */
+  public static String trimSubfield0Value(String zeroValue) {
     if (nonNull(zeroValue)) {
       var slashIndex = zeroValue.lastIndexOf('/');
       if (slashIndex != -1) {
@@ -21,6 +27,13 @@ public class FieldUtils {
     return zeroValue;
   }
 
+  /**
+   * Returns subfield $0 value with baseUrl from sourceFile.
+   *
+   * @param naturalId  Authority natural id as {@link String}
+   * @param sourceFile Authority source file as {@link AuthoritySourceFile}
+   * @return subfield $0 value as {@link String}
+   */
   public static String getSubfield0Value(String naturalId, AuthoritySourceFile sourceFile) {
     var subfield0Value = "";
     if (nonNull(naturalId) && nonNull(sourceFile)) {
@@ -29,11 +42,18 @@ public class FieldUtils {
     return subfield0Value + naturalId;
   }
 
-  public static String getSubfield0Value(Map<UUID, AuthoritySourceFile> files, String naturalId) {
-    var sourceFile = files.values().stream()
+  /**
+   * Search for source file by naturalId and returns subfield $0 value.
+   *
+   * @param sourceFiles Map of authority source files,
+   *                    Where Key - sourceFileId as {@link UUID}, Value - sourceFile {@link AuthoritySourceFile}
+   * @param naturalId   Authority natural id
+   * @return subfield $0 value as {@link String}
+   */
+  public static String getSubfield0Value(Map<UUID, AuthoritySourceFile> sourceFiles, String naturalId) {
+    var sourceFile = sourceFiles.values().stream()
       .filter(file -> file.codes().stream().anyMatch(naturalId::startsWith))
-      .findFirst()
-      .orElse(null);
+      .findFirst().orElse(null);
 
     if (nonNull(sourceFile)) {
       return getSubfield0Value(naturalId, sourceFile);
