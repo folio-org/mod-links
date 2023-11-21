1d5b9bec0c76098567f71bc2a608cf794bcde00e
Mukhiddin Yusupov <mukhiddin_yusupov@epam.com>
2023-11-21 13:01:04 +0500

    modelinks-139: refactor authority and authority archive classes

--- src/main/java/org/folio/entlinks/domain/entity/AuthorityArchive.java
+++ src/main/java/org/folio/entlinks/domain/entity/AuthorityArchive.java
@@ -2,11 +2,9 @@ package org.folio.entlinks.domain.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
-import java.util.Objects;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.hibernate.Hibernate;
 
 @Getter
 @Setter
@@ -14,21 +12,4 @@ import org.hibernate.Hibernate;
 @ToString
 @Table(name = "authority_archive")
 public class AuthorityArchive extends AuthorityBase {
-
-  @Override
-  public int hashCode() {
-    return Objects.hashCode(getId());
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) {
-      return false;
-    }
-    Authority that = (Authority) o;
-    return getId() != null && Objects.equals(getId(), that.getId());
-  }
 }
