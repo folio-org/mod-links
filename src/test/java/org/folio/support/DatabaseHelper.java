9704216c61b5b48a4b0e8bb358e16f65e9311116
GeloPakDev1 <Oleg_Pak1@epam.com>
2024-02-20 18:58:46 +0500

    test sequence deletion

--- src/test/java/org/folio/support/DatabaseHelper.java
+++ src/test/java/org/folio/support/DatabaseHelper.java
@@ -61,14 +61,14 @@ public class DatabaseHelper {
 
   public void saveAuthoritySourceFile(String tenant, AuthoritySourceFile entity) {
     var sourceType = getDbPath(tenant, AUTHORITY_SOURCE_FILE_SOURCE_TYPE);
-    var sqlValues = "(?,?,?::" + sourceType + ",?,?,?,?,?,?,?,?,?)";
+    var sqlValues = "(?,?,?::" + sourceType + ",?,?,?,?,?,?,?,?)";
     var sql = "INSERT INTO " + getDbPath(tenant, AUTHORITY_SOURCE_FILE_TABLE)
       + " (id, name, source, type, base_url_protocol, base_url, hrid_start_number, created_date, updated_date,"
-      + "created_by_user_id, updated_by_user_id, sequence_name) VALUES " + sqlValues;
+      + "created_by_user_id, updated_by_user_id) VALUES " + sqlValues;
     jdbcTemplate.update(sql, entity.getId(), entity.getName(),
       entity.getSource().name(), entity.getType(), entity.getBaseUrlProtocol(), entity.getBaseUrl(),
       entity.getHridStartNumber(), entity.getCreatedDate(), entity.getUpdatedDate(), entity.getCreatedByUserId(),
-      entity.getUpdatedByUserId(), entity.getSequenceName());
+      entity.getUpdatedByUserId());
   }
 
   public void createSequence(String sequenceName, int startNumber) {
