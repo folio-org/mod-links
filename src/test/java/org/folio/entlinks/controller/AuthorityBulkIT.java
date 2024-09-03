1f2e656a0f76facc3a4ad7dc4e502672bcda4423
viacheslav_kolesnyk <Viacheslav_Kolesnyk@epam.com>
2024-09-03 14:15:59 +0200

    Rework integration test

--- src/test/java/org/folio/entlinks/controller/AuthorityBulkIT.java
+++ src/test/java/org/folio/entlinks/controller/AuthorityBulkIT.java
@@ -14,7 +14,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import org.folio.entlinks.domain.dto.AuthorityBulkRequest;
 import org.folio.s3.client.FolioS3Client;
@@ -43,7 +42,7 @@ class AuthorityBulkIT extends IntegrationTestBase {
   private @Autowired ResourceLoader loader;
 
   @BeforeAll
-  static void prepare() throws IOException {
+  static void prepare() {
     setUpTenant(true);
   }
 
@@ -54,33 +53,7 @@ class AuthorityBulkIT extends IntegrationTestBase {
     var filename = s3Client.write("parentLocation/filePath/fileName", resource.getInputStream());
     var dto = new AuthorityBulkRequest(filename);
 
-    tryPost(authorityEndpoint() + "/bulk", dto)
-      .andExpect(status().isOk())
-      .andExpect(jsonPath("$.errorsNumber", is(2)));
-
-    assumeTrue(databaseHelper.countRows(AUTHORITY_TABLE, TENANT_ID) == 1);
-    var list = s3Client.list("parentLocation/filePath/");
-    assertThat(list)
-      .hasSize(3)
-      .containsExactly("parentLocation/filePath/fileName",
-        "parentLocation/filePath/fileName_errors",
-        "parentLocation/filePath/fileName_failedEntities");
-    var errors = new BufferedReader(new InputStreamReader(s3Client.read("parentLocation/filePath/fileName_errors")))
-        .lines()
-        .toList();
-    assertThat(errors)
-      .hasSize(2)
-      .anyMatch(s -> s.contains("constraint [authority_storage_source_file_id_foreign_key]"))
-      .anyMatch(s -> s.contains("Unexpected json parsing exception")); //invalid UUID for noteTypeId
-  }
-
-  @Test
-  @DisplayName("POST: create new Authority with defined ID and retries on s3 upload exception")
-  void createAuthority_positive_entityCreatedWithProvidedId_withS3Retries() throws Exception {
-    var resource = loader.getResource("classpath:test-data/authorities/bulkAuthorities");
-    var filename = s3Client.write("parentLocation/filePath/fileName", resource.getInputStream());
-    var dto = new AuthorityBulkRequest(filename);
-
+    //trigger spring retry once, then - call real methods
     doThrow(IllegalStateException.class)
       .doCallRealMethod()
       .when(s3Client).upload(any(), any());
@@ -104,6 +77,7 @@ class AuthorityBulkIT extends IntegrationTestBase {
       .anyMatch(s -> s.contains("constraint [authority_storage_source_file_id_foreign_key]"))
       .anyMatch(s -> s.contains("Unexpected json parsing exception")); //invalid UUID for noteTypeId
 
+    //1'st time with exception triggering spring retry, then - 2 regular calls
     verify(s3Client, times(3)).upload(any(), any());
   }
 
